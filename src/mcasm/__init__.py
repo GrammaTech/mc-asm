eb500f3c2c02650cef9df8e16c12bb116eaee830
Joe Ranieri <jranieri@grammatech.com>
2022-08-17 14:44:48 +0000

    Optimize the Python dispatch performance

--- src/mcasm/__init__.py
+++ src/mcasm/__init__.py
@@ -1,6 +1,4 @@
-from mcasm._core import Assembler, ParserState, X86Syntax, mc
+from mcasm._core import Assembler, ParserState, Streamer, X86Syntax, mc
 from mcasm.version import __version__  # noqa: F401
-
-from .streamer import Streamer
 
 __all__ = ["Assembler", "ParserState", "Streamer", "X86Syntax", "mc"]
